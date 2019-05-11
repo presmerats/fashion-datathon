fcb2a99b4f50f444a0c965e753578174dd6c3f1f
Anthony Nixon <anixon604@gmail.com>
2019-05-11 18:16:04 +0200

    API query for faces completed, added parameter for filename and working on matching faces to body

--- body_detect.py
+++ body_detect.py
@@ -7,43 +7,71 @@ import operator
 api_key = os.environ["fpAPI"]
 api_secret = os.environ["fpAPISecret"]
 
-def get_bodies():
+def match_faces_to_bodies(faces, bodies):
+    for face in faces:
+        faceLocation = face['face_rectangle']
+    return 0
 
-    # files={'image_file': open("./frames/family-photography-bambini-025.jpg", 'rb')}
-    # headers = {'content-type': 'application/x-www-form-urlencoded'}
-    # body = {
-    #         "api_key": api_key,
-    #         "api_secret": api_secret,
-    #         "return_attributes": 'gender,upper_body_cloth,lower_body_cloth'
-    #         "image_file": files['image_file']
-    #         }
-    # #print(body)
+def get_faces(filemame='./frames/shopCouple.jpg'):
+    # gender
+    # age
+    # smiling
+    # headpose
+    # facequality
+    # blur
+    # eyestatus
+    # emotion
+    # ethnicity
+    # beauty
+    # mouthstatus
+    # eyegaze
+    # skinstatus
+    attributes = "gender,age,smiling,emotion,ethnicity,beauty"
 
     files = {
         'api_key': (None, api_key),
         'api_secret': (None, api_secret),
-        'image_file': ('image_file.jpg', open('./frames/family-photography-bambini-025.jpg', 'rb')),
+        'image_file': ('image_file.jpg', open('./frames/shopCouple.jpg', 'rb')),
+        'return_attributes': (None, attributes),
+    }
+
+    try: # connection for POST
+        req = requests.post('https://api-us.faceplusplus.com/facepp/v3/detect', files=files)
+
+        return json.loads(req.text)
+
+    except Exception:
+        print("Error")
+
+def get_bodies(filemame='./frames/shopCouple.jpg'):
+
+    files = {
+        'api_key': (None, api_key),
+        'api_secret': (None, api_secret),
+        'image_file': ('image_file.jpg', open(filename, 'rb')),
         'return_attributes': (None, 'gender,upper_body_cloth,lower_body_cloth'),
     }
 
 
     try: # connection for POST
         req = requests.post('https://api-us.faceplusplus.com/humanbodypp/v1/detect', files=files)
-        print(req.json())
 
         return json.loads(req.text)
 
-    except Exception as e:
+    except Exception:
         print("Error")
 
 
+faces = get_faces()
 people = get_bodies()
-print(people)
 
+# math the faces and bodies
+match_faces_to_bodies(faces['faces'], people['humanbodies'])
+
+# NOTE that for upper / lower body colors we also have the option to grab RGB
 # for human in people['humanbodies']:
 #     sex = human['attributes']['gender']
-#     sex = 
-#     upper_body_cloth_color = human['attributes']['upper_body_cloth_color']
-#     lower_body_cloth_color = human['attributes']['lower_body_cloth_color']
-
-#     print(f"sex: {sex}, upper: {upper_body_cloth_color}, lower: {lower_body_cloth_color}")
+#     upper_body_cloth_color = human['attributes']['upper_body_cloth']#['upper_body_cloth_color']
+#     lower_body_cloth_color = human['attributes']['lower_body_cloth']#['lower_body_cloth_color']
+#     rectangle = human['humanbody_rectangle']
+#     #print(f"rectangle: {rectangle}, sex: {sex}, upper: {upper_body_cloth_color}, lower: {lower_body_cloth_color}")
